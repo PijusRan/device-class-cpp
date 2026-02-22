0777df8dcd8a473d1d6cd47f375e4b637053e191
PijusRan <pijusranonis@gmail.com>
2026-02-22 22:56:30 +0200

    Identification + Numeration

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
  - [C++ Constructors and Destructors](https://www.w3schools.in/cplusplus/constructors-destructors/)
  - ['this' pointer in C++](https://www.geeksforgeeks.org/cpp/this-pointer-in-c/)
  - [Execption Handling in C++](https://www.geeksforgeeks.org/cpp/exception-handling-c/)
- - 
+ - [Map in C++ STL](https://www.geeksforgeeks.org/cpp/map-associative-containers-the-c-standard-template-library-stl/)
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <string>
 #include <iomanip>
+#include <string>
 #include <map>
 using namespace std;
 
+// Tipai apytiksliai paimti is Xiaomi parduotuves puslapio
 enum deviceType{
     light,
     environment,
@@ -14,23 +15,59 @@ enum deviceType{
     hub,
     other
 };
-// Tipai apytiksliai paimti is Xiaomi parduotuves puslapio
-
+map<int, string> typeTitles = {
+    {0, "Light"},
+    {1, "Environment"},
+    {2, "Appliance"},
+    {3, "Display / Audio"},
+    {4, "Communication"},
+    {5, "Security"},
+    {6, "Hub"},
+    {7, "Other"}
+};
 
 class device{
+    // --- LAUKAI ---
     private:
         string name;
         deviceType type;
         bool isOn;
         int power;
 
+        // Numeracija
+        static unsigned int n;
+        unsigned int id;
+        void incN(){ n++; }
+        void decN(){ n--; }
+
     public:
-    // Setters
+    // --- CONSTRUCTOR ---
+    device(string name, deviceType type, int power){
+        setName(name);
+        setType(type);
+        setStatus(false);
+        setPower(power);
+
+        id = n;
+        incN();
+        
+        cout << "Device created.\n";
+    }
+
+    // --- DESTRUCTOR ---
+    ~device(){
+        decN();
+
+        cout << "Device removed.\n";
+    }
+
+    // --- SETTERS ---
         string getName(){ return name; }
         deviceType getType(){ return type; }
         bool getStatus(){ return isOn; }
         int getPower(){ return power; }
-    // Getters
+
+    // --- GETTERS ---
         void setName(string to){
             if(to.empty()){
                 throw invalid_argument("Name cannot be empty");
@@ -41,38 +78,12 @@ class device{
         void setStatus(bool to){ isOn = to; }
         void setPower(int to){ power = to; }
 
-    // Constructor / Destructor
-    device(string name, deviceType type, int power){
-        setName(name);
-        setType(type);
-        setStatus(false);
-        setPower(power);
-        
-        cout << "Device created.\n";
-    }
-    ~device(){
-        cout << "Device removed.\n";
-    }
-
-    // toString
+    // --- TOSTRING ---
     string toString(){
-        map<int, string> typeTitles = {
-            {0, "Light"},
-            {1, "Environment"},
-            {2, "Appliance"},
-            {3, "Display / Audio"},
-            {4, "Communication"},
-            {5, "Security"},
-            {6, "Hub"},
-            {7, "Other"}
-        };
-
-        return "device("+name+", "+typeTitles[type]+", "+to_string(power)+"W , "+ (isOn ? "On" : "Off") + ")\n";
+        return "device["+id+"]("+name+", "+typeTitles[type]+", "+to_string(power)+"W , "+ (isOn ? "On" : "Off") + ")\n";
     }
 };
 
 int main(){
-    device dev("Lamp", light, 5);
-
-    cout << dev.toString();
+    
 }
