9c5884fa5009f5750879b16f5bfc1316dc28809a
PijusRan <pijusranonis@gmail.com>
2026-02-24 11:04:11 +0200

    Panaikinti saltiniai

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 #include <string>
 #include <map>
+#include <assert.h>
+#include <vector>
 using namespace std;
 
 // Tipai apytiksliai paimti is Xiaomi parduotuves puslapio
@@ -15,15 +17,15 @@ enum deviceType{
     hub,
     other
 };
-map<int, string> typeTitles = {
-    {0, "Light"},
-    {1, "Environment"},
-    {2, "Appliance"},
-    {3, "Display / Audio"},
-    {4, "Communication"},
-    {5, "Security"},
-    {6, "Hub"},
-    {7, "Other"}
+map<deviceType, string> typeTitles = {
+    {light, "Light"},
+    {environment, "Environment"},
+    {appliance, "Appliance"},
+    {display_audio, "Display / Audio"},
+    {communication, "Communication"},
+    {security, "Security"},
+    {hub, "Hub"},
+    {other, "Other"}
 };
 
 class device{
@@ -35,39 +37,44 @@ class device{
         int power;
 
         // Numeracija
+        static unsigned int nextID;
         static unsigned int n;
         unsigned int id;
-        void incN(){ n++; }
+        void incN(){ n++; nextID++; }
         void decN(){ n--; }
 
     public:
     // --- CONSTRUCTOR ---
+    
+
     device(string name, deviceType type, int power){
         setName(name);
         setType(type);
         setStatus(false);
         setPower(power);
 
-        id = n;
+        id = nextID;
         incN();
         
-        cout << "Device created.\n";
+        cout << "Device ["+to_string(id)+"] created.\n";
     }
 
     // --- DESTRUCTOR ---
     ~device(){
         decN();
 
-        cout << "Device removed.\n";
+        cout << "Device ["+to_string(id)+"] removed.\n";
     }
 
-    // --- SETTERS ---
+    // --- GETTERS ---
         string getName(){ return name; }
         deviceType getType(){ return type; }
         bool getStatus(){ return isOn; }
         int getPower(){ return power; }
+        unsigned int getID(){ return id; }
+        unsigned int getN(){ return n; }
 
-    // --- GETTERS ---
+    // --- SETTERS ---
         void setName(string to){
             if(to.empty()){
                 throw invalid_argument("Name cannot be empty");
@@ -80,10 +87,61 @@ class device{
 
     // --- TOSTRING ---
     string toString(){
-        return "device["+id+"]("+name+", "+typeTitles[type]+", "+to_string(power)+"W , "+ (isOn ? "On" : "Off") + ")\n";
+        return "device["+to_string(id)+"]("+name+", "+typeTitles[type]+", "+to_string(power)+"W , "+ (isOn ? "On" : "Off") + ")";
     }
 };
+unsigned int device::n = 0;
+unsigned int device::nextID = 0;
 
 int main(){
-    
+    device d1 = device("Kitchen Light", light, 5);
+
+    // --- Unit Tests ---
+
+    // Informacija
+    assert(d1.toString() == "device[0](Kitchen Light, Light, 5W , Off)");
+    assert(d1.getID() == 0);
+
+    // Pavadinimas
+    assert(d1.getName() == "Kitchen Light");
+    d1.setName("Kitchen Hub");
+    assert(d1.getName() == "Kitchen Hub");
+
+    // Tipas
+    assert(d1.getType() == light);
+    d1.setType(hub);
+    assert(d1.getType() == hub);
+
+    // Busena
+    assert(d1.getStatus() == false);
+    d1.setStatus(true);
+    assert(d1.getStatus() == true);
+
+    // Galia
+    assert(d1.getPower() == 5);
+    d1.setPower(15);
+    assert(d1.getPower() == 15);
+
+    // Validacija
+    try {         
+        d1.setName("");
+    } 
+    catch (invalid_argument e) {}
+    assert(d1.getName() != "");
+
+    // Nauju irengininiu identifikavimas
+    vector<device*> D;
+    for(int i = 1; i <= 3; i++){
+        D.push_back(new device("device "+to_string(i), other, 0));
+        assert((*D[D.size()-1]).getID() == i);
+        2
+    }
+    assert(d1.getN() == 4);
+    for(int i = 0; i < 3; i++){
+        delete(D[i]);
+    };
+    assert(d1.getN() == 1);
+
+
+    cout << "Tests passed.\n";
 }
